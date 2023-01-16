e4c7f66fac3abb9254893d4206a5ce0fe1de1305
P-edroP-arno <pedroparno@gmail.com>
2023-01-15 22:01:27 -0300

    1º teste

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../pages/About';
+
+describe('Testando o componente <About/>', () => {
+  test('Paǵina contém um h2 com o texto "About Pokédex"', () => {
+    renderWithRouter(<About />);
+
+    const title = screen.getByRole('heading', { name: /about pokédex/i });
+
+    expect(title).toBeInTheDocument();
+  });
+
+  test('Página contém dois parágrafos com o texto "Pokédex"', () => {
+    renderWithRouter(<About />);
+
+    const texts = screen.getAllByText(/pokémon/i);
+
+    expect(texts).toHaveLength(2);
+  });
+
+  test('Página contém a seguinte imagem de uma Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const pokedexPic = screen.getByRole('img', { name: /pokédex/i });
+
+    expect(pokedexPic).toBeInTheDocument();
+    expect(pokedexPic.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
